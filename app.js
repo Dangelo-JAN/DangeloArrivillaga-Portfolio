0a324e618a584acb23164ed35d0edd8762df5a64
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-03-01 08:56:18 -0400

    Fix Eslint errors

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const hamburguer = document.querySelector('.hamburguer');
+
 const menu = document.querySelector('.nav-desktop');
+
 const close = document.querySelector('.close-icon');
 
+hamburguer.addEventListener('click', () =>
 
+  menu.classList.toggle('active'),
+);
 
+close.addEventListener('click', () =>
 
-hamburguer.addEventListener('click',function() {
-    menu.classList.toggle('active');
-});
+  menu.classList.toggle('active'),
+);
 
-close.addEventListener('click', function (){
-    menu.classList.toggle('active');
-});
+document.querySelectorAll('.li-desktop').forEach( (n) => n.addEventListener('click', () =>
 
-document.querySelectorAll('.li-desktop').forEach( (n) => n.addEventListener('click', function(){
-    menu.classList.toggle('active');
+  menu.classList.toggle('active'),
+));
 
-}))
+document.querySelectorAll('..arrow-right-style').forEach( (x) => x.addEventListener('click', () =>
 
-document.querySelectorAll('..arrow-right-style').forEach( (x) => x.addEventListener('click', function(){
-    menu.classList.toggle('active');
-
-}))
+  menu.classList.toggle('active'),
+));
